900d5d4f2ae9dede276da5aee35581f812fded08
Şeyma Atmaca <54944249+SeymaAtmaca@users.noreply.github.com>
2021-08-26 14:47:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
 
     char metin[15];
 
-    printf("Lutfen prefix idafenizi giriniz : ");
+    printf("Prefix expression : ");
     gets(metin);
 
     QUEUE* queue1=createQueue();
@@ -18,7 +18,7 @@ int main()
     {
         if((queue1->count==1 && queue2->count==0)||(queue1->count==0 && queue2->count==1))
             break;
-        if(queue1->count>1 && queue2->count==0) // Q1 den Q2 ye kontrollü aktarim
+        if(queue1->count>1 && queue2->count==0) // Q1 den Q2 ye kontrollÃ¼ aktarim
             transfer(queue1,queue2);
         else if(queue2->count>1 && queue2->count ==0 ) //Q2 den Q1 e aktarim
             transfer(queue2,queue1);
